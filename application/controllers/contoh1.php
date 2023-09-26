197e6e795655d1f213a1a2a2fb867b858b15628a
hafilachsan <hafilachsan11@gmail.com>
2023-09-26 20:13:56 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Hafil Achsan
+            Saya Tinggal di daerah Mampang
+            Olahraga yang saya sukai adalah sepakbola";
+
+    }
+}
